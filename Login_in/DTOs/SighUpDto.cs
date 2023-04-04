2452f1e477b1b4e7b869174d13331965e2874ca5
Yura <urakost427@gmail.com>
2023-04-04 16:49:40 +0300

    Created DTOs for login in and signing up

---
+++ Login_in/DTOs/SighUpDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Login_in.DTOs
+{
+    public class SighUpDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
