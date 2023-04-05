956693beb68509dbfe6f9a5258de6c122950d275
Yura <urakost427@gmail.com>
2023-04-05 21:04:35 +0300

    Added attributes

--- Login_in/DTOs/SighUpDto.cs
+++ Login_in/DTOs/SighUpDto.cs
@@ -1,10 +1,19 @@
-﻿namespace Login_in.DTOs
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Login_in.DTOs
 {
     public class SighUpDto
     {
+        
+        [Required]
+        [DisplayName("Username")]
         public string UserName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string PasswordConfirm { get; set; }
     }
 }
